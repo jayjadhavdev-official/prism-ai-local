8c5fb34ef5534d38e5ef4d0be9642e9a3efaeff8
TheJamer <jadhav.jay2010@gmail.com>
2026-06-16 18:21:26 +0100

    choice.py rectified.

--- choice.py
+++ choice.py
@@ -5,8 +5,15 @@ def choice():
     
     if choose.lower() == "api key":
         api_key = input("\nPaste your OpenAI API Key here:\n\nKEY: ")
+        return "api", api_key
+        
     elif choose.lower() == ".gguf":
         model_key = input("\nPaste directory to your .gguf file here:\n\nPATH: ")
+        return "local", model_key
+    
+    else:
+       print("Invalid choice. Please restart.")
+       return None, None 
 
 if __name__ == "__main__":
     print("This is the choice.py file.")
